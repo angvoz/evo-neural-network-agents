24501de36a137547998e5b69bd3b56df1f89d32d
Andrew Gvozdev <angvoz.dev@gmail.com>
2020-09-16 11:06:28 -0400

    Tweak normalizing angle and speed, do not allow negative speed

--- src/main/java/com/lagodiuk/agent/evolution/NeuralNetworkDrivenAgent.java
+++ src/main/java/com/lagodiuk/agent/evolution/NeuralNetworkDrivenAgent.java
@@ -220,10 +220,10 @@ public class NeuralNetworkDrivenAgent extends Agent {
 	}
 
 	private double normalizeSpeed(double speed) {
-		double abs = Math.abs(speed);
-		if (abs > maxSpeed) {
-			double sign = Math.signum(speed);
-			speed = sign * (abs - (Math.floor(abs / maxSpeed) * maxSpeed));
+		if (speed > maxSpeed) {
+			speed = maxSpeed;
+		} else if (speed < 0) {
+			speed = 0;
 		}
 		return speed;
 	}
@@ -232,7 +232,7 @@ public class NeuralNetworkDrivenAgent extends Agent {
 		double abs = Math.abs(angle);
 		if (abs > maxDeltaAngle) {
 			double sign = Math.signum(angle);
-			angle = sign * (abs - (Math.floor(abs / maxDeltaAngle) * maxDeltaAngle));
+			angle = sign * maxDeltaAngle;
 		}
 		return angle;
 	}
