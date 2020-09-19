266049c334449297de8dd1808557f20423d59ef9
Andrew Gvozdev <angvoz.dev@gmail.com>
2020-09-19 00:46:39 -0400

    Added radius for agents

--- src/main/java/com/lagodiuk/agent/Food.java
+++ src/main/java/com/lagodiuk/agent/Food.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2012 Yuriy Lagodiuk
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 package com.lagodiuk.agent;
 
 public class Food implements AbstractAgent {
+	private static double RADIUS = 2;
 
 	private double x;
 
@@ -44,6 +45,11 @@ public class Food implements AbstractAgent {
 	@Override
 	public void setY(double y) {
 		this.y = y;
+	}
+
+	@Override
+	public double getRadius() {
+		return RADIUS;
 	}
 
 	@Override
